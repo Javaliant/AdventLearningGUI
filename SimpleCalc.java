ff1f945ba277ebf4a4e12cedc703e4792b003edc
Luigi Vincent <luigivincenthr@gmail.com>
2014-07-14 01:17:41 -0400

    Update SimpleCalc.java
    Added a few comments.

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -1,4 +1,4 @@
-/* Author: Luigi Vincent
+    /* Author: Luigi Vincent
 Practicing Java Swing button and button events; Window that does simple calculation on two numbers
 TO DO: Figure out why command prompt show so many messages when fields are empty & stop it
 */
@@ -11,75 +11,79 @@ import java.text.DecimalFormat;
 
 public class SimpleCalc extends JFrame {
 
-	SimpleCalc (String name) {
-		super(name);
-		
-		setLayout(new FlowLayout());
-		
-		JTextField result = new JTextField(10);
-		JTextField num1 = new JTextField(5);
-		JTextField num2 = new JTextField(5);
-		result.setEditable(false);
+    SimpleCalc (String name) {
+        super(name);
 
-		JButton b_add = new JButton("Add");
-		JButton b_sub = new JButton("Subtract");
-		JButton b_mul = new JButton("Multiply");
-		JButton b_div = new JButton("Divide");
+        setLayout(new FlowLayout());
 
-		ActionListener calculator = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					if (num1.getText().length() == 0 || num2.getText().length() == 0) {
-						throw new Exception();
-					}
-				}
-				catch (Exception ex){
-					JOptionPane.showMessageDialog(null, "Field missing", "Warning", JOptionPane.WARNING_MESSAGE);
-				}
-				
-				Double alpha = Double.parseDouble(num1.getText());
-				Double beta = Double.parseDouble(num2.getText());
-				Double omega = null;
-				DecimalFormat f = new DecimalFormat("##.00");
+        // Fields for input and output
+        JTextField result = new JTextField(10);
+        JTextField num1 = new JTextField(5);
+        JTextField num2 = new JTextField(5);
+        result.setEditable(false);
 
-				if (e.getSource() == b_add) {
-					omega = alpha + beta;
-				}
-				else if (e.getSource() == b_sub) {
-					omega = alpha - beta;
-				}
-				else if (e.getSource() == b_mul) {
-					omega = alpha * beta;
-				}
-				else if (e.getSource() == b_div) {
-					omega = alpha / beta;
-				}
-				result.setText(f.format(omega).toString());
-			}
-		};
+        // Buttons for operations
+        JButton b_add = new JButton("Add");
+        JButton b_sub = new JButton("Subtract");
+        JButton b_mul = new JButton("Multiply");
+        JButton b_div = new JButton("Divide");
 
-		b_add.addActionListener(calculator);
-		b_sub.addActionListener(calculator);
-		b_mul.addActionListener(calculator);
-		b_div.addActionListener(calculator);
+        // Listener to handle operations
+        ActionListener calculator = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (num1.getText().length() == 0 || num2.getText().length() == 0) {
+                        throw new Exception();
+                    }
+                }
+                catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, "Field missing", "Warning", JOptionPane.WARNING_MESSAGE);
+                }
 
-		add(new JLabel("First Number"));
-		add(num1);
-		add(new JLabel("Second Number"));
-		add(num2);
-		add(new JLabel("Result"));
-		add(result);
-		add(b_add);
-		add(b_sub);
-		add(b_mul);
-		add(b_div);
-	}
-	public static void main(String[] args) {
-		SimpleCalc frame = new SimpleCalc("Exercise 4");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 100);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+                Double alpha = Double.parseDouble(num1.getText());
+                Double beta = Double.parseDouble(num2.getText());
+                Double omega = null;
+                DecimalFormat f = new DecimalFormat("##.00");
+
+                if (e.getSource() == b_add) {
+                    omega = alpha + beta;
+                }
+                else if (e.getSource() == b_sub) {
+                    omega = alpha - beta;
+                }
+                else if (e.getSource() == b_mul) {
+                    omega = alpha * beta;
+                }
+                else if (e.getSource() == b_div) {
+                    omega = alpha / beta;
+                }
+                result.setText(f.format(omega).toString());
+            }
+        };
+       // Register buttons to event listener
+        b_add.addActionListener(calculator);
+        b_sub.addActionListener(calculator);
+        b_mul.addActionListener(calculator);
+        b_div.addActionListener(calculator);
+
+       // Add everything to frame + labels    
+        add(new JLabel("First Number"));
+        add(num1);
+        add(new JLabel("Second Number"));
+        add(num2);
+        add(new JLabel("Result"));
+        add(result);
+        add(b_add);
+        add(b_sub);
+        add(b_mul);
+        add(b_div);
+    }
+    public static void main(String[] args) {
+        SimpleCalc frame = SimpleCalc("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 100);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
