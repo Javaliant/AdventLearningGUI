dd512be4759ece1ca89a641ad0cbe2ed825c0e45
Luigi Vincent <luigivincenthr@gmail.com>
2014-07-15 00:02:04 -0400

    Update SimpleCalc.java
    Adjusted parameters to conventional form. Added properly functional empty field check, and non-number field check.

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -1,13 +1,11 @@
 /* Author: Luigi Vincent
 Practicing Java Swing button and button events; Window that does simple calculation on two numbers
-TO DO: Figure out why command prompt show so many messages when fields are empty & stop it
 */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.DecimalFormat;
-
 
 public class SimpleCalc extends JFrame {
 
@@ -23,49 +21,51 @@ public class SimpleCalc extends JFrame {
         result.setEditable(false);
 
         // Buttons for operations
-        JButton b_add = new JButton("Add");
-        JButton b_sub = new JButton("Subtract");
-        JButton b_mul = new JButton("Multiply");
-        JButton b_div = new JButton("Divide");
+        JButton add = new JButton("Add");
+        JButton sub = new JButton("Subtract");
+        JButton mul = new JButton("Multiply");
+        JButton div = new JButton("Divide");
 
         // Listener to handle operations
         ActionListener calculator = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    if (num1.getText().length() == 0 || num2.getText().length() == 0) {
-                        throw new Exception();
-                    }
-                }
-                catch (Exception ex){
+                // Checks if fields are empty
+                if (num1.getText().length() == 0 || num2.getText().length() == 0) {
                     JOptionPane.showMessageDialog(null, "Field missing", "Warning", JOptionPane.WARNING_MESSAGE);
+                    return;
                 }
+                // Checks if fields are numbers
+                if (isNaN(num1.getText()) || isNaN(num2.getText())) {
+                    JOptionPane.showMessageDialog(null, "Inputs must be numbers", "Warning", JOptionPane.WARNING_MESSAGE);
+                    return;
+                } 
 
                 Double alpha = Double.parseDouble(num1.getText());
                 Double beta = Double.parseDouble(num2.getText());
                 Double omega = null;
                 DecimalFormat f = new DecimalFormat("##.00");
 
-                if (e.getSource() == b_add) {
+                if (e.getSource() == add) {
                     omega = alpha + beta;
                 }
-                else if (e.getSource() == b_sub) {
+                else if (e.getSource() == sub) {
                     omega = alpha - beta;
                 }
-                else if (e.getSource() == b_mul) {
+                else if (e.getSource() == mul) {
                     omega = alpha * beta;
                 }
-                else if (e.getSource() == b_div) {
+                else if (e.getSource() == div) {
                     omega = alpha / beta;
                 }
                 result.setText(f.format(omega).toString());
             }
         };
        // Register buttons to event listener
-        b_add.addActionListener(calculator);
-        b_sub.addActionListener(calculator);
-        b_mul.addActionListener(calculator);
-        b_div.addActionListener(calculator);
+        add.addActionListener(calculator);
+        sub.addActionListener(calculator);
+        mul.addActionListener(calculator);
+        div.addActionListener(calculator);
 
        // Add everything to frame + labels    
         add(new JLabel("First Number"));
@@ -74,16 +74,28 @@ public class SimpleCalc extends JFrame {
         add(num2);
         add(new JLabel("Result"));
         add(result);
-        add(b_add);
-        add(b_sub);
-        add(b_mul);
-        add(b_div);
+        add(add);
+        add(sub);
+        add(mul);
+        add(div);
     }
+
     public static void main(String[] args) {
         SimpleCalc frame = new SimpleCalc("Simple Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 100);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    // Method that checks whether or not input field is a number
+    public static boolean isNaN(String str) {  
+        try {  
+            double d = Double.parseDouble(str);  
+        }  
+        catch(NumberFormatException nfe) {
+            return true;  
+        }  
+      return false;  
     }
 }
